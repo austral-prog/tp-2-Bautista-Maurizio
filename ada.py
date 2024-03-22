cc8f8afc33654339fd276ce834bc8a4e00b8f69b
Bautista-Maurizio <83047373+Bautista-Maurizio@users.noreply.github.com>
2024-03-22 12:40:26 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print (first_name.lower(),last_name.lower())
+    Name_title = f"{first_name.title()} {last_name.title()}"
+    print (Name_title)
+    Name_upper = f"{first_name.upper()} {last_name.upper()}"
+    print(Name_upper)
+    Name_tab = f"\t{first_name.lower()} {last_name.lower()}"
+    print (Name_tab)
